a4a13a08d395d75f6067e9824d00fd2d18fedcc4
Élder Ramos <elderrramos@gmail.com>
2025-04-05 00:26:46 -0300

    refactor: migrar código para usar importações ES6 nos modelos e no index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const { Client, LocalAuth } = require("whatsapp-web.js");
-const qrcode = require("qrcode-terminal");
-const messagesServiceClass = require("./services/messages.service").default;
-const utilServiceClass = require("./services/utils.service").default;
-const { Sequelize } = require("sequelize");
+import { Client, LocalAuth } from "whatsapp-web.js";
+import { generate } from "qrcode-terminal";
+import messagesServiceClass from "./services/messages.service";
+import utilServiceClass from "./services/utils.service";
+import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
   "postgres://user:password@postgres:5432/postgres"
@@ -36,7 +36,7 @@ const client = new Client({
 });
 
 client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
+  generate(qr, { small: true });
 });
 
 client.on("ready", () => {
